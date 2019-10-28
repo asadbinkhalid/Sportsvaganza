29ab8e59545de4034762096a7ce4d37e408d6a5f
Asad bin Khalid <54309765+asadbinkhalid@users.noreply.github.com>
2019-10-28 23:32:58 +0500

    first commit

--- app/src/main/java/com/asad/sportsvaganza/Activity2_gamesList.java
+++ app/src/main/java/com/asad/sportsvaganza/Activity2_gamesList.java
@@ -2,13 +2,18 @@ package com.asad.sportsvaganza;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Activity2_gamesList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity2_games_list);
+
+
     }
 }
