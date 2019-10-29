adde8b8a042e26f222e42b377785e82773aa09b5
asadbinkhalid <asadbkpk@gmail.com>
2019-10-30 00:40:57 +0500

    Football results RecyclerView almost done

--- app/src/main/java/com/asad/sportsvaganza/Fragments/FootballResultsFragment.java
+++ app/src/main/java/com/asad/sportsvaganza/Fragments/FootballResultsFragment.java
@@ -7,7 +7,10 @@ import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.asad.sportsvaganza.Adapters.FootballResultsAdapter;
 import com.asad.sportsvaganza.R;
 
 public class FootballResultsFragment extends Fragment {
@@ -28,9 +31,16 @@ public class FootballResultsFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_football_results, viewGroup, false);
+        View view = inflater.inflate(R.layout.fragment_football_results, viewGroup, false);
+        RecyclerView football_fixtures_list = view.findViewById(R.id.football_results_list);
+        football_fixtures_list.setLayoutManager(new LinearLayoutManager(getContext()));
+        String[] matches = {"match 1", "match 2", "match 3", "match 4", "match 5", "match 6", "match 7", "match 8", "match 9", "match 10", "match 11", "match 12", "match 13", "match 14", "match 15"};
+        football_fixtures_list.setAdapter(new FootballResultsAdapter(matches));
 
+
+        return view;
     }
 
 }
