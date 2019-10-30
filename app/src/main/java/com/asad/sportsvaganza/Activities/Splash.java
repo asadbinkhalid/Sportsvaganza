e681d18ffb2a242a14b8e17d0032f5112ec79b42
ikhawaja-hassan <ahmed.alliance1@gmail.com>
2019-10-31 00:10:06 +0500

    Splash and fragments added

---
+++ app/src/main/java/com/asad/sportsvaganza/Activities/Splash.java
@@ -0,0 +1,39 @@
+package com.asad.sportsvaganza.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.asad.sportsvaganza.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+
+// Using handler with postDelayed called runnable run method
+
+            @Override
+
+            public void run() {
+
+                Intent i = new Intent(Splash.this, Activity_Login_Main.class);
+
+                startActivity(i);
+
+                // close this activity
+
+                finish();
+
+            }
+
+        }, 3*1000);
+        setContentView(R.layout.activity_splash);
+    }
+
+}
