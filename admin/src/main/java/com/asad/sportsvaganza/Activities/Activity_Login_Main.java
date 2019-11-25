bcb1272198e93eda8372bdf836a2ad87639056dd
ikhawaja-hassan <ahmed.alliance1@gmail.com>
2019-11-25 17:10:49 +0500

    Firebase login added (ADMIN ONLY)

--- admin/src/main/java/com/asad/sportsvaganza/Activities/Activity_Login_Main.java
+++ admin/src/main/java/com/asad/sportsvaganza/Activities/Activity_Login_Main.java
@@ -1,24 +1,34 @@
 package com.asad.sportsvaganza.Activities;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.text.TextUtils;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.asad.sportsvaganza.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Activity_Login_Main extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         Button signup = findViewById(R.id.button_login);
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,12 +57,28 @@ public class Activity_Login_Main extends AppCompatActivity {
                 }
 
                 if (check) {
-                    Intent intent = new Intent(Activity_Login_Main.this, Activity2_gamesList.class);
-                    startActivity(intent);
+                    mAuth.signInWithEmailAndPassword(strUserName, strPassword)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+
+                                        Intent intent = new Intent(Activity_Login_Main.this, Activity2_gamesList.class);
+                                        startActivity(intent);
+                                    }
+                                    else {
+                                        Toast.makeText(getApplicationContext(), "Login failed! Please try again later", Toast.LENGTH_LONG).show();
+
+                                    }
+                                }
+                            });
+
                 }
-                else
-                    return;
+
+
             }
         });
+        FirebaseApp.initializeApp(getApplicationContext());
+        mAuth = FirebaseAuth.getInstance();
     }
 }
